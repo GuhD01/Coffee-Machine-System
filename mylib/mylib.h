c4b0fbacba3e863f2bb8df2fb6267c63b403a8b4
GuhD <88705355+GuhD01@users.noreply.github.com>
2023-01-11 23:48:19 +0700

    Update mylib.h

--- mylib/mylib.h
+++ mylib/mylib.h
@@ -1,17 +1,6 @@
 #ifndef MYLIB_H
 #define MYLIB_H
 
-#define OFF 0
-#define WAITING 1
-#define BREWING 2
-#define DISPENSING 3
-
-#define INPUT_OFF 0
-#define INPUT_ON 1
-#define INPUT_START_PROCESS 2
-
-
-void update_coffee_machine(int *state, int input);
-const char *get_state_string(int state);
+void states(int s0, int s1, int F0, int F1, int* S0, int* S1);
 
 #endif
